50ac892344e59cdb1459c10b887f4367c35104f2
lisafranz98 <lisa.franz98@t-online.de>
2023-09-10 11:04:28 +0200

    - table with mock data added - sort and filter for table added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const table = document.querySelector('table');
+    let isAscending = true;
+
+    table.querySelector('thead').addEventListener('click', (e) => {
+        // Überprüfen Sie, ob das angeklickte Element das data-sortable Attribut hat
+        if (!e.target.hasAttribute('data-sortable')) return;
+
+        const colIndex = Array.from(e.target.parentElement.children).indexOf(e.target);
+        const rows = Array.from(table.querySelectorAll('tbody tr'));
+
+        const compareFunction = isNaN(parseFloat(rows[0].cells[colIndex].textContent.trim()))
+            ? (a, b) => a.cells[colIndex].textContent.trim().localeCompare(b.cells[colIndex].textContent.trim())
+            : (a, b) => parseFloat(a.cells[colIndex].textContent.trim()) - parseFloat(b.cells[colIndex].textContent.trim());
+
+        const sortedRows = rows.sort(compareFunction);
+
+        if (!isAscending) {
+            sortedRows.reverse();
+        }
+
+        table.querySelector('tbody').append(...sortedRows);
+        isAscending = !isAscending;
+    });
+});
+
+document.getElementById('filterEmitter').addEventListener('keyup', function() {
+    const filterValue = this.value.toLowerCase();
+    const rows = document.querySelectorAll('table tbody tr');
+
+    rows.forEach(row => {
+        const rowText = row.textContent.toLowerCase();
+        row.style.display = rowText.includes(filterValue) ? "" : "none";
+    });
+});
+
 document.getElementById('languageSelector').addEventListener('click', function(event) {
     if (event.target && event.target.matches("a.dropdown-item")) {
         const selectedLang = event.target.getAttribute('data-lang');
